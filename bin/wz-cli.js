0bbf8c68edf69ace7772547364a50d6a6aa1e52f
wayley <hb_wangzheng@163.com>
2019-08-23 17:17:18 +0800

    modified in 2019-08-23 17:17

--- bin/wz-cli.js
+++ bin/wz-cli.js
@@ -12,7 +12,7 @@ const {
 
 function initAction() {
   inquirer.prompt(init).then(answers => {
-    console.log('初始化成功', answers);
+    console.log('Init succeed ');
   })
 
 }
@@ -25,7 +25,6 @@ program
   .description('list files in current working directory') //给出list这个命令的描述
   .option('-a, --all', 'Whether to display hidden files') //设置list这个命令的参数
   .action(function (options) { //list命令的实现体
-    var fs = require('fs');
     //获取当前运行目录下的文件信息
     fs.readdir(process.cwd(), function (err, files) {
       var list = files;
@@ -41,16 +40,9 @@ program
 program
   .command('init')
   .description('初始化')
-  .action((env, options) => {
-    console.log(env);
-    console.log('---------------');
+  .action((options) => {
     console.log(options);
-    // initAction()
+    initAction()
   })
 
-program
-  .command('*')
-  .action(function (env) {
-    console.log('deploying "%s"', env);
-  });
 program.parse(process.argv);

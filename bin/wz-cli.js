b2ef81544d6fafe244915a24440a027e4bed3274
wayley <hb_wangzheng@163.com>
2019-08-28 16:59:19 +0800

    improve the cli

--- bin/wz-cli.js
+++ bin/wz-cli.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const PATH = require('path');
 const program = require('commander');
-const inquirer = require('inquirer')
 const pkg = require('../package.json');
 
 const {
-  nameInput,
-  htmlContain
-} = require('../lib/prompt.config')
-
+  createAction,
+  initAction
+} = require('../lib/action.config')
 const _ = require('../lib/utils')
 
 program
@@ -19,58 +15,26 @@ program
 
 program
   .command('create')
-  .option('-a,--all')
   .arguments('<app-name>')
-  .description('create a new project')
+  .option('-a,--all', 'Whether to init all files')
+  .description('create a new app project')
   .action((appName, options) => {
-
-    const appPath = PATH.join(process.cwd(), `/${appName}`)
-
-    const htmlPath_r = PATH.join(__dirname, '../templates/index.html')
-    const htmlPath_w = PATH.join(appPath, `index.html`)
-
-    const gitignorePath_r = PATH.join(__dirname, '../templates/.gitignore')
-    const gitignorePath_w = PATH.join(appPath, `.gitignore`)
-
-    _.mkdir(appPath)
-      .then(path => {
-        const htmlTask = _.copyTask(htmlPath_r, htmlPath_w, /Document/, appName)
-        const gitignoreTask = _.copyTask(gitignorePath_r, gitignorePath_w)
-
-        return new Promise((resolve, reject) => {
-          Promise.all([htmlTask, gitignoreTask]).then(list => {
-            resolve(list)
-          }).catch(err => {
-            reject(err)
-          })
-        })
-
-      })
-      .catch(error => {})
+    createAction(appName)
   })
-
-// program
-//   .command('init')
-//   .arguments('<cmd> [env]')
-//   .description('初始化') //命令的描述
-//   .option('-a,--allww', 'Whether to show all options') //命令的参数
-//   .action(function (cmd, env) {
-//     console.log(cmd, env);
-//     return
-//     // inquirer.prompt(nameInput).then(answers => {
-//     //   console.log(options, 'Init succeed ', answers);
-//     //   inquirer.prompt(htmlContain).then(answers => {
-//     //     console.log('htmlContain succeed ', answers);
-//     //   })
-//     // })
-//   })
+program
+  .command('init')
+  // .arguments('[env]')
+  .description('initialize a new app project') //命令的描述
+  .option('-a,--all', 'Whether to init all files') //命令的参数
+  .action(function (options) {
+    initAction()
+  })
 // program
 //   .command('list')
 //   .description('list files in current working directory')
 //   .option('-a, --all', 'Whether to display hidden files')
 //   .action(function (options) {
 //     console.log(options['all']);
-
 //     fs.readdir(process.cwd(), function (err, files) {
 //       var list = files;
 //       if (!options['all']) { //检查用户是否给了--all或者-a的参数，如果没有，则过滤掉那些以.开头的文件

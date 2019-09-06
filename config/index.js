a8f3922ce5edac95b99778d85efd346dbae02553
wayley <hb_wangzheng@163.com>
2019-09-06 16:05:40 +0800

    add global configs

---
+++ config/index.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: wzheng(hb_wangzheng@163.com)
+ * @Github: https://github.com/wayley
+ * @Company: Fih-ACKN
+ * @Date: 2019-09-06 14:08:17
+ * @LastEditors: wzheng(hb_wangzheng@163.com)
+ * @LastEditTime: 2019-09-06 14:12:11
+ * @Description:
+ */
+const frameworkList = [{
+    id: 0,
+    name: 'Native JavaScript'
+  },
+  {
+    id: 1,
+    name: 'Vue'
+  },
+  {
+    id: 2,
+    name: 'React'
+  },
+  {
+    id: 3,
+    name: 'Angular'
+  },
+  {
+    id: 4,
+    name: 'React Native'
+  },
+  {
+    id: 5,
+    name: 'AngularJS'
+  },
+  {
+    id: 100,
+    name: 'other frameworks in manual way'
+  }
+]
+
+
+module.exports = {
+  frameworkList
+}
